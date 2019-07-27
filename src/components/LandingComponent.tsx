917335937b88cab2e59a14b827ded3571e130963
Zurez <zurez4u@gmail.com>
2019-07-27 14:49:15 +0530

    Added Semantic

---
+++ src/components/LandingComponent.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import SingleQuestionComponent from './SingleQuestionComponent';
+export default class LandingComponent extends Component {
+    render() {
+        return (
+    
+           <SingleQuestionComponent />
+        )
+    }
+}
---
+++ src/components/SingleQuestionComponent.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleQuestionComponent extends Component {
+    render() {
+        return (
+            <div>
+                Hey
+            </div>
+        )
+    }
+}
